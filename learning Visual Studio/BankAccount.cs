a58c69567e506ab67e50275a38afcee13f906e73
JOSEPH CHARBONNEAU <JOSEPH.CHARBONNEAU@otech.edu>
2022-02-16 10:20:01 -0700

    added bank account code

---
+++ learning Visual Studio/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BankAccount
+{
+    // INSTANCE VARIABLES... (each instance of this "BankAccount" class will have its own "name")
+    private string name;  // variable names begin with lowercase;
+                          // PRIVATE keeps access to this variable "internal only" (good for information hiding),
+                          // outside instances of this class can only affect the value of "name" if one of the methods here has that capability
+
+    public void SetName(string accountName)  // methods begin with uppercase; "VOID" indicates that this method WILL NOT RETURN any value
+    {
+        name = accountName;  // assigns the parameter value from "accountName" (a local variable available only to this method) into the class's INSTANCE VARIABLE: "name"
+    }
+
+    public string GetName()  // PUBLIC methods can be called from outside instances of this class
+    {
+        return name;  // here's how this public method can SEND BACK OUT the class's private instance variable: "name"
+    }
+}
--- learning Visual Studio/MyFirstProgram.cs
+++ learning Visual Studio/MyFirstProgram.cs
@@ -19,74 +19,95 @@ using System;
 
             // new classes can be made by "inheriting" existing class models, and then personalizing some aspect(s) of the new class (example: a convertible class can be made by inheriting a car class)
 
-            // Unified Modeling Language (UML) is a graphical scheme for blueprinting the requirements and design decisions of a given project
-            
+            // Unified Modeling Language (UML) is a graphical scheme for blueprinting/summarizing the attributes and operations related to the requirements and design decisions of a given project
+
             // .NET Framework = a collection of standard prebuilt classes (I/O, db, networking, mobile, file access, web comm, etc) that help a developer build quickly (and avoid re-inventing the wheel)
 
             // VisualStudio "apps" -- built from "solutions" -- built from "projects"
             // File > New > Project ...Installed > Templates > Visual C# ...Windows > Console Application
 
+            // TYPES: string, bool, byte, sbyte, char, short, ushort, int, uint, long, ulong, float, double, decimal
 
 
             // BREAKPOINT EXAMPLE
             // ----------------------------------------------------------
-            Console.WriteLine("BREAKPOINT example: ");
-            int myVal = 50;
-            Console.WriteLine(myVal);
-            Console.WriteLine("");
+            //            Console.WriteLine("BREAKPOINT example: ");
+            //            int myVal = 50;
+            //            Console.WriteLine(myVal);
+            //            Console.WriteLine("");
 
 
             // ARITHMETIC EXAMPLE
             // ----------------------------------------------------------
-            Console.WriteLine("ARITHMETIC example: ");
-            int number1; int number2;
-            int mySum, myProduct, myDifference, myQuotient;
+            //            Console.WriteLine("ARITHMETIC example: ");
+            //            int number1; int number2;
+            //            int mySum, myProduct, myDifference, myQuotient;  // variables defined with their name and a TYPE
 
-            Console.Write("Enter first integer: ");
-            number1 = int.Parse(Console.ReadLine());
+            //            Console.Write("Enter first integer: ");
+            //            number1 = int.Parse(Console.ReadLine());  // Parse converts strings to a number (int, in this case)
 
-            Console.Write("Enter second integer: ");
-            number2 = int.Parse(Console.ReadLine());
+            //            Console.Write("Enter second integer: ");
+            //            number2 = int.Parse(Console.ReadLine());
 
-            mySum = number1 + number2;
-            myProduct = number1 * number2;
-            myDifference = number1 - number2;
-            myQuotient = number1 / number2;
+            //            mySum = number1 + number2; 
+            //            myProduct = number1 * number2;
+            //            myDifference = number1 - number2;
+            //            myQuotient = number1 / number2;
 
-            Console.WriteLine($"Sum = {mySum}");
-            Console.WriteLine($"Product = {myProduct}");
-            Console.WriteLine($"Difference = {myDifference}");
-            Console.WriteLine($"Quotient = {myQuotient}");
-            Console.WriteLine("");
+            //            Console.WriteLine($"Sum = {mySum}");  // INTERPOLATED STRING, starts with a $ symbol and then "interpolates" the bracketed expression as part of the overall string
+            //            Console.WriteLine($"Product = {myProduct}");
+            //            Console.WriteLine($"Difference = {myDifference}");
+            //            Console.WriteLine($"Quotient = {myQuotient}");
+            //            Console.WriteLine("");
 
 
             // OUTPUT EXAMPLE
             // ----------------------------------------------------------
-            Console.WriteLine("OUTPUT Example: ");
-            int num1=1; int num2=2; int num3 = 3; int num4 = 4;
+            //            Console.WriteLine("OUTPUT Example: ");
+            //            int num1=1; int num2=2; int num3 = 3; int num4 = 4;
 
-            Console.WriteLine("1 2 3 4");
+            //            Console.WriteLine("1 2 3 4");
 
-            Console.Write("1 ");
-            Console.Write("2 ");
-            Console.Write("3 ");
-            Console.Write("4\n");
+            //            Console.Write("1 ");
+            //            Console.Write("2 ");
+            //            Console.Write("3 ");
+            //            Console.Write("4\n");
 
-            Console.WriteLine($"{num1} {num2} {num3} {num4}");
-            Console.WriteLine("");
+            //            Console.WriteLine($"{num1} {num2} {num3} {num4}");
+            //            Console.WriteLine("");
 
 
             // MATH EXAMPLE
             // ----------------------------------------------------------
-            Console.WriteLine("MATH Example: ");
-            int radiusIN; double myPI = Math.PI;
+            //            Console.WriteLine("MATH Example: ");
+            //            int radiusIN; double myPI = Math.PI;
 
-            Console.Write("Enter the radius of a circle (integer): ");
-            radiusIN = int.Parse(Console.ReadLine());
+            //            Console.Write("Enter the radius of a circle (integer): ");
+            //            radiusIN = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Diameter: {(radiusIN * 2)}, Circumference: {((radiusIN * 2) * myPI)}, Area: {(myPI * (radiusIN * radiusIN))}");
+            //            Console.WriteLine($"Diameter: {(radiusIN * 2)}, Circumference: {((radiusIN * 2) * myPI)}, Area: {(myPI * (radiusIN * radiusIN))}");
+            //            Console.WriteLine("");
+
+
+            // BANK ACCOUNT (Chapter 4)
+            // class properties: Name, Blanace
+            // class methods: Deposit, Withdraw
+            // ----------------------------------------------------------
+            BankAccount myAccount = new BankAccount();  // "new" is an object-creation expression, and the Type ("Account") should match the class we're instantiating
+            Console.WriteLine($"Initial name is: {myAccount.GetName()}");  // Account has a method called "GetName", so the new object "myAccount" will hve this as well
+
+            Console.Write("Enter the name: ");
+            string theName = Console.ReadLine();
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
             Console.WriteLine("");
 
+
+            // Hit any key...
+            // ----------------------------------------------------------
+            Console.WriteLine("PRESS <ENTER> TO QUIT");
+            Console.ReadLine();
         }
 
     }
